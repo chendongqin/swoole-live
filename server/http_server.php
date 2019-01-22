9ca6304e06ef168d866820dd441ce1d92a610d43
Donging <913294974@qq.com>
2019-01-22 23:41:36 +0800

    C:/Program Files/Git/

--- server/http_server.php
+++ server/http_server.php
@@ -22,23 +22,23 @@ $http->on('WorkerStart',function (swoole_server $server,$worker_id){
 
 $http->on('request',function ($request ,$response) use($http){
     if(!empty($request->get)){
-        $_GET =[];
+//        $_GET =[];
         $_GET = $request->get;
     }
     if(!empty($request->post)){
-        $_POST =[];
+//        $_POST =[];
         $_POST = $request->post;
     }
     if(!empty($request->cookie)){
-        $_COOKIE =[];
+//        $_COOKIE =[];
         $_COOKIE = $request->cookie;
     }
     if(!empty($request->files)){
-        $_FILES =[];
+//        $_FILES =[];
         $_FILES = $request->files;
     }
     if(!empty($request->server)){
-        $_SERVER =[];
+//        $_SERVER =[];
         foreach ($request->server as $key =>$value){
             $_SERVER[strtoupper($key)] = $value;
         }
