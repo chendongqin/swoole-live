ce9e9b71fd80dbf5d68b864c3510800d013a1f28
Donging <913294974@qq.com>
2019-01-22 18:55:37 +0800

    C:/Program Files/Git/

--- server/http_server.php
+++ server/http_server.php
@@ -11,11 +11,38 @@ $http->set(
     [
         'enable_static_handler'=>true,
         'document_root'=>__DIR__.'/../public/static',
+        'worker_num'=>5,
     ]
 );
 
+$http->on('WorkerStart',function (swoole_server $server,$worker_id){
+    define('APP_PATH',__DIR__.'/../application/');
+    require __DIR__ . '/../thinkphp/base.php';
+});
+
 $http->on('request',function ($request ,$response){
-    $response->end('sss'.json_encode($request->get));
+    if(!empty($request->get)){
+        $_GET = $request->get;
+    }
+    if(!empty($request->post)){
+        $_POST = $request->post;
+    }
+    if(!empty($request->cookie)){
+        $_COOKIE = $request->cookie;
+    }
+    if(!empty($request->files)){
+        $_FILES = $request->files;
+    }
+    if(!empty($request->server)){
+        foreach ($request->server as $key =>$value){
+            $_SERVER[strtoupper($key)] = $value;
+        }
+    }
+    ob_start();
+    think\Container::get('app')->run()->send();
+    $res = ob_get_contents();
+    ob_end_clean();
+    $response->end($res);
 });
 
 $http->start();
