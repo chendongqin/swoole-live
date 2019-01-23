95a66a2fe38a60076bc52674ec7bfa29c86a2dc8
Donging <913294974@qq.com>
2019-01-23 22:01:33 +0800

    update/

---
+++ application/index/controller/Login.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dongq
+ * Date: 2019/1/23
+ * Time: 21:56
+ */
+
+namespace app\index\controller;
+
+use think\console\command\make\Controller;
+use think\Session;
+
+class Login extends Controller{
+
+    public function index(){
+        $mobile = request()->get('phone_num');
+        $code = request()->get('phone_num');
+        $virefy = Session::get('liveLogin'.$mobile);
+        if(!$virefy){
+            $data = array('msg'=>'验证码已过期','status'=>false,'code'=>2000,'data'=>[]);
+            return json($data);
+        }
+        if($code != $virefy){
+            $data = array('msg'=>'验证码不正确','status'=>false,'code'=>2001,'data'=>[]);
+            return json($data);
+        }
+        $data = array('msg'=>'登陆成功','status'=>true,'code'=>0,'data'=>[]);
+        return json($data);
+    }
+
+}
---
+++ application/index/controller/Send.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dongq
+ * Date: 2019/1/23
+ * Time: 21:42
+ */
+
+namespace app\index\controller;
+
+use think\console\command\make\Controller;
+use think\Session;
+
+class Send extends Controller{
+
+
+    public function index($mobile = ''){
+        if(empty($mobile)){
+            $data = array('msg'=>'手机号不能为空','status'=>false,'code'=>1000,'data'=>[]);
+            return json($data);
+        }
+        $code = '0000';
+        Session::set('liveLogin'.$mobile, $code, 120);
+        $data = array('msg'=>'发送成功','status'=>true,'code'=>0,'data'=>[]);
+        return json($data);
+    }
+
+}
